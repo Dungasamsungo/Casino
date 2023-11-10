fa08ed9b3633c783617121f5d7f834a336522e49
dungasamsungo <sampsonngobi@gmail.com>
2023-11-10 17:39:35 -0300

    Initiated the jason packages for the prompt sync

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit money
+// 2. Determine number of lines to bet on 
+// 3. Collect a bet amount 
+// 4. Spin the slot 
+// 5. Check if the player won 
+// 6. Give the winner 
+// 7. Play again 
