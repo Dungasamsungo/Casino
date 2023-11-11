26852f538029e088377717dd8ca761c7890484c2
dungasamsungo <sampsonngobi@gmail.com>
2023-11-11 00:40:55 -0300

    Wrote the three functions for deposit, number of lines, bet amount prompts

--- project.js
+++ project.js
@@ -5,3 +5,55 @@
 // 5. Check if the player won 
 // 6. Give the winner 
 // 7. Play again 
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while(true){
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+    
+        if (isNaN(numberDepositAmount) ||numberDepositAmount <= 0 ){
+            console.log("Invalid deposit amount. Try again.");
+        } else{
+            return numberDepositAmount
+        }
+
+    }; 
+    
+};
+
+const getNumberOfLines = () => {
+    while(true){
+        const lines = prompt("Enter the number of lines to bet on (1 to 3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) ||numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid number of lines. Try again.");
+        } else{
+            return numberOfLines;
+        }
+    }
+};
+
+const getBet = (balance, lines) => {
+    while(true){
+        const bet = prompt("Enter the total bet per line: ");
+        const numberbet = parseFloat(bet);
+
+        if (isNaN(numberbet) ||numberbet <= 0 || numberbet > balance / lines) {
+            console.log("Invalid bet. Try again.");
+        } else{
+            return numberbet;
+        }
+    };
+
+
+};
+
+let balance = deposit()
+const numberOfLines = getNumberOfLines()
+const bet = getBet(balance, numberOfLines)
+
+
+
